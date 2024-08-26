ae21c9995862edd9ac6d303bdcad1b92ecff7027
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-08-26 18:22:04 +0530

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/Navbar/NavBar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ front-end/src/components/Navbar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>NavBar</div>
+  )
+}
+
+export default NavBar
